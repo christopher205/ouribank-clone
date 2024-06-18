0c9060136f6d1785cc777abf5209276b497f991f
christopher205 <christopherwillians7@gmail.com>
2024-06-18 13:10:35 -0300

    add mais uma seção

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, HostListener, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   constructor(private render : Renderer2) {}
